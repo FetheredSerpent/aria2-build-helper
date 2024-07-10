0ec32723db48294829c45f335f14579849b5fd6a
FetheredSerpent <144615238+FetheredSerpent@users.noreply.github.com>
2024-07-11 00:22:10 +0530

    Update runner script

--- run.sh
+++ run.sh
@@ -109,7 +109,7 @@ cd ..
 mkdir build
 git clone -b $ARIA2_VERSION --depth 1 https://github.com/aria2/aria2 && \
 mv mingw-config aria2 && cd aria2 && chmod +x mingw-config && autoreconf -i && \
-./mingw-config && make -j$(nproc) && \ $HOST-strip src/aria2c.exe && \
-./mingw-release && mv *.zip ../build
+./mingw-config && make -j$(nproc) && $HOST-strip src/aria2c.exe && \
+chmod +x mingw-release && ./mingw-release && mv *.zip ../build
 
 cd ..
